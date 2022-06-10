55b3c1fc197ee447e4714e80c4d73f5402d02b4b
Oleg Skinderev <>
2022-06-10 21:38:47 +0300

    add assert status code

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,21 +2,25 @@
 
 def test_ping(myserver):
     res = myserver.get('/ping')
+    assert res.status_code == 200
     assert res.text == 'pong'
 
 
 def test_pong(myserver):
     res = myserver.get('/pong')
+    assert res.status_code == 200
     assert res.text == 'ping'
 
 
 def test_hello(myserver):
     name = 'John'
     res = myserver.get(f'/hello/{name}')
+    assert res.status_code == 200
     assert res.text == f'Hello {name}!'
 
 
 def test_goodbye(myserver):
     name = 'John'
     res = myserver.get(f'/goodbye/{name}')
+    assert res.status_code == 200
     assert res.text == f'Goodbye {name}!'
